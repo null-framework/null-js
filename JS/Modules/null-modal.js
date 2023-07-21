b1873f61f72bddabf0aea778976f2f57002d2a90
Null Framework <131310053+null-framework@users.noreply.github.com>
2023-07-22 00:56:16 +0300

    Update null-modal.js
    Add helper functions (null_modal_open, null_modal_close)

--- JS/Modules/null-modal.js
+++ JS/Modules/null-modal.js
@@ -10,70 +10,82 @@
  * Initializes a null modal component.
  */
 
-function null_modal() {
-
-  // Select all the elements with the appropriate data attributes and classes
-  const nullOpenModalButton = document.querySelectorAll('[data-null-modal-target]');
-  const nullCloseModalButton = document.querySelectorAll('[data-close-button]');
-  const nullModal = document.querySelectorAll('.null-modal-container');
-  const nullBody = document.body;
-
-  // Add click event listeners to all the close buttons
-  nullCloseModalButton.forEach(function(button) {
-    button.addEventListener('click',() => {
-      const modal = button.closest(".null-modal-container");
-      // Remove the 'null-modal-body-overflow-hidden' class from the body
-      nullBody.classList.remove("null-modal-body-overflow-hidden");
-      // Call the function to close the modal
-      nullCloseModal(modal);
-    })
-  });
-
-  // Add click event listeners to all the open modal buttons
-  nullOpenModalButton.forEach(function(button) {
-    button.addEventListener('click',() => {
-      const modal = document.querySelector(button.dataset.nullModalTarget);
-      // Add the 'null-modal-body-overflow-hidden' class to the body
-      nullBody.classList.add("null-modal-body-overflow-hidden");
-      // Call the function to open the modal
-      nullOpenModal(modal);
-    })
-  });
-
-  // Function to add the 'active' class to a modal to show it
-  function nullOpenModal(modal){
-    if(modal == null) return;
-    modal.classList.add('active');
-  }
-
-  // Function to remove the 'active' class from a modal to hide it
-  function nullCloseModal(modal) {
+// Function to add the 'active' class to a modal to show it
+function openModal(modal) {
     if (modal == null) return;
-    modal.classList.remove('active');
-  }
+    modal.classList.add('active');
+}
 
-  // Add a click event listener to the window to close the modal when user clicks outside the modal (overlay)
-  window.addEventListener('click', nullOutSideClick);
+// Global variable to store modal close callbacks
+const modalCloseCallBack = {};
 
-  // Function to close the modal when user clicks outside the modal (overlay)
-  function nullOutSideClick(e) {
+function null_modal() {
+    // Select all the elements with the appropriate data attributes and classes
+    const openModalButton = document.querySelectorAll('[data-null-modal-target]');
+    const closeModalButton = document.querySelectorAll('[data-null-close-button]');
+    const modal = document.querySelectorAll('.null-modal-container');
+    const body = document.body;
 
-    for (let i = 0; i < nullModal.length; i++) {
+    // Add click event listeners to all the close buttons
+    closeModalButton.forEach(function (button) {
+        button.addEventListener('click', () => {
+            const modal = button.closest(".null-modal-container");
 
-      if (e.target == nullModal[i]) {
-        // Remove the 'active' class from the modal to hide it
-        nullModal[i].classList.remove('active');
-        // Remove the 'null-modal-body-overflow-hidden' class from the body
-        nullBody.classList.remove("null-modal-body-overflow-hidden");
+            const targetId = "#" + modal.getAttribute("id");
 
-      }
-    }
+            // call set_modal_close_callback instead of closeModal
+            set_modal_close_callback(targetId);
+        });
+    });
 
-  }
+    // Add click event listeners to all the open modal buttons
+    openModalButton.forEach(function (button) {
+        button.addEventListener('click', () => {
+            const modal = document.querySelector(button.dataset.nullModalTarget);
+            // Add the 'modal-body-overflow-hidden' class to the body
+            body.classList.add("null-modal-body-overflow-hidden");
+            // Call the function to open the modal
+            openModal(modal);
+        });
+    });
 
-
+    // Add a click event listener to the window to close the modal when user clicks outside the modal (overlay)
+    window.addEventListener('click', e => {
+        modal.forEach(modalItem => {
+            if (e.target === modalItem) {
+                const targetId = `#${modalItem.getAttribute("id")}`;
+                set_modal_close_callback(targetId);
+            }
+        });
+    });
 }
 
 null_modal();
+
+function set_modal_close_callback(targetId) {
+    const modal = document.querySelector(targetId);
+    if (modal != null) {
+        modal.classList.remove('active');
+        document.body.classList.remove("null-modal-body-overflow-hidden");
+        // Check if a callback exists for this modal, if so, call it
+        if (modalCloseCallBack[targetId]) {
+            modalCloseCallBack[targetId]();
+        }
+    }
+}
+
+// Function to manually trigger a modal
+function null_modal_open(targetId) {
+    const modal = document.querySelector(targetId);
+    document.body.classList.add("null-modal-body-overflow-hidden");
+    openModal(modal);
+}
+
+// Function to set the callback for a modal close event
+function null_modal_close(targetId, callBack) {
+    if (callBack && typeof callBack === "function") {
+        modalCloseCallBack[targetId] = callBack;
+    }
+}
 
 /* End Modal */
